44843545c126f984dadce01b818bd4c26d224dff
anna-choo <anna.choo@ntu.edu.sg>
2023-10-02 13:53:34 +0800

    trying to create test data, next step to consider which marker to test

--- sandbox/pilotTest.py
+++ sandbox/pilotTest.py
@@ -13,15 +13,8 @@ from utils.GET_metafileDetails import metafile
 
 
 
-class read_c3d_file:
-    def __init__(self, c3d_path):
-        # load c3d file
-        self.c = c3d(c3d_path)
-
-        # get all markers name in the file and their coordinates
-        self.name_of_markers = self.c["parameters"]["POINT"]["DESCRIPTIONS"]["value"]
-        self.coordinates_of_markers = self.c["data"]["points"][0:3,:,:]
-
+class c3d_file:
+    def __init__(self):
         self.mapping_anatomical_group_to_marker = {
             'head':[
             'RHEAD',
@@ -173,6 +166,17 @@ class read_c3d_file:
             'pink':['head', 'shoulder', 'torso', 'pelvis', 'arms', 'wrist', 'hand', 'thigh', 'calves', 'foot'],
             'yellow':['head', 'shoulder', 'torso', 'pelvis', 'arms', 'wrist', 'thigh', 'calves', 'foot']
             }
+        
+
+    
+    def read_c3d(self, c3d_path):
+        # load c3d file
+        self.c = c3d(c3d_path)
+
+        # get all markers name in the file and their coordinates
+        self.name_of_markers = self.c["parameters"]["POINT"]["DESCRIPTIONS"]["value"]
+        self.coordinates_of_markers = self.c["data"]["points"][0:3,:,:]
+
 
         # function that will be used to capture frames of the missing markers
     def ranges(self, i):
@@ -188,9 +192,9 @@ class read_c3d_file:
         anatomical_group = self.mapping_color_to_anatomical_group[self.mapping_task_to_color[task]]
 
         # rearrange the markers that are highly correlated together
-        self.problematic_markers = collections.defaultdict()
-        self.transformed_marker_sequence = []
-        self.transformed_data =  None
+        problematic_markers = collections.defaultdict()
+        transformed_marker_sequence = []
+        transformed_data =  None
         for group in anatomical_group:
             for marker in self.mapping_anatomical_group_to_marker[group]:
                 
@@ -201,62 +205,128 @@ class read_c3d_file:
                     # check if marker has missing timeframes
                     missing_timeframes = list(self.ranges(np.argwhere(np.isnan(self.coordinates_of_markers[0,idx:idx+1,:]))[:,1]))
                     if len(missing_timeframes) != 0:
-                        self.problematic_markers[marker] = missing_timeframes
+                        problematic_markers[marker] = missing_timeframes
                     
                     # start forming the sequence according to self.mapping_color_to_anatomical_group
-                    if self.transformed_data is None:
-                        self.transformed_data = self.coordinates_of_markers[:,idx:idx+1,:]
+                    if transformed_data is None:
+                        transformed_data = self.coordinates_of_markers[:,idx:idx+1,:]
                     else:
-                        self.transformed_data = np.concatenate((self.transformed_data, self.coordinates_of_markers[:,idx:idx+1,:]), axis = 1)
-                    self.transformed_marker_sequence.append(marker)
+                        transformed_data = np.concatenate((transformed_data, self.coordinates_of_markers[:,idx:idx+1,:]), axis = 1)
+                    transformed_marker_sequence.append(marker)
 
                 else:
                     # find out what are the missing markers, if any
-                    self.problematic_markers[marker] = "ALL"
+                    problematic_markers[marker] = "ALL"
+        return transformed_data, transformed_marker_sequence, problematic_markers
 
-        return self.transformed_data, self.transformed_marker_sequence, self.problematic_markers  
+    def preprocess(self, data, minmax = False, znorm = False):
+        missing_markers_mask =  np.isnan(data).any(axis = 1)
+        
+        # Invert the mask to get the valid timeframes
+        valid_timeframes_mask = ~missing_markers_mask.any(axis=0)
 
-    def exclude_timeframe_with_missing_data(self):
-        self.cropped_transformed_data = None
-        for timeframe in problematic_markers.keys():
-            crop_start = timeframe[0]
-            crop_end = timeframe[1]
+        # Use the mask to drop timeframes with missing markers
+        cleaned_data = data[:, :, valid_timeframes_mask]
+        
+        # minmax or z-score normalisation (standardisation) of the data
+        manipulate_data = copy.copy(data)
+        axes = ['x', 'y', 'z']
 
-            self.cropped_transformed_data = self.transformed_data[:,:,0:crop_start]
+        # for inversion of scaling later
+        scaling = collections.defaultdict()
+
+        for i, axis in enumerate(axes):
+            data_in_each_axis = manipulate_data[i:i+1,:,:]
+            if znorm == True:
+                # record down the standardisation factors for z-score normalisation 
+                scaling["avg_" + axis] = np.mean(data_in_each_axis)
+                scaling["sd_" + axis] = np.std(data_in_each_axis)
+                # z-score normalise the data
+                manipulate_data[i:i+1,:,:] = ((data_in_each_axis - scaling["avg_" + axis]) / scaling["sd_" + axis])
+            
+
+            if minmax == True:
+                # record down the scaling factors for minmax
+                scaling["min_" + axis] = np.min(data_in_each_axis)
+                scaling["max_" + axis] = np.max(data_in_each_axis)
+                # minmax the normalised data
+                manipulate_data[i:i+1,:,:] = ((data_in_each_axis - scaling["min_" + axis]) / (scaling["max_" + axis] - scaling["min_" + axis]))
+        
+        if len(scaling) == 0:
+            print("No scaling done")
+        
+        return cleaned_data, scaling
+    #     self.cropped_transformed_data = None
+    #     for timeframe in problematic_markers.keys():
+    #         crop_start = timeframe[0]
+    #         crop_end = timeframe[1]
+
+    #         self.cropped_transformed_data = transformed_data[:,:,0:crop_start]
 
 
 
 if __name__ == "__main__":
 
-    # train_dir = r"C:\Users\anna\Documents\gitHub\NTU\InterpolationModel\data\c3d\train"
-    # test_dir = r"C:\Users\anna\Documents\gitHub\NTU\InterpolationModel\data\c3d\test"
+    train_dir = r"C:\Users\anna\Documents\gitHub\NTU\InterpolationModel\data\c3d\train"
+    test_dir = r"C:\Users\anna\Documents\gitHub\NTU\InterpolationModel\data\c3d\test"
 
-    dir = r"C:\Users\Administrator\Documents\gitHub\NTU\InterpolationModel\data\train"
+    # dir = r"C:\Users\Administrator\Documents\gitHub\NTU\InterpolationModel\data\train"
     # file = r"C:\Users\Administrator\Documents\gitHub\NTU\InterpolationModel\data\SN268\SN268_0027_10m_05_c3d.c3d"
     METAFILES_STORAGE_DIR = r"Z:\DataCollection\AbilityData\Normative\Processed\MetaFiles"
-    subject_list = ["SN196", "SN200", "SN202", "SN268", "SN273", "SN279", "SN281", "SN292", "SN301", "SN328"]
+    train_subject_list = ["SN196", "SN200", "SN202", "SN268", "SN273", "SN279", "SN281", "SN292", "SN301"]
+    test_subject_list = ["SN328"]
 
     # train_dir = r"C:\Users\Administrator\Documents\gitHub\NTU\InterpolationModel\data"
     # test_dir = r"C:\Users\Administrator\Documents\gitHub\NTU\InterpolationModel\data"
 
     # instantiate GET_metafiledetails
     metafile_fn = metafile()
-    metadata = metafile_fn.gather_metafile(METAFILES_STORAGE_DIR, subject_list)
-    print(metadata)
+    train_metadata = metafile_fn.gather_metafile(METAFILES_STORAGE_DIR, train_subject_list)
+    train_metadata = train_metadata.reset_index()
+
+    test_metadata = metafile_fn.gather_metafile(METAFILES_STORAGE_DIR, test_subject_list)
+    test_metadata = test_metadata.reset_index()
+
+    # instantiate c3d_file
+    c3d_function = c3d_file()
     test_task = "10m"
-    data = None
-    all_problematic_markers = [] 
-    for file_name in metadata[metadata["task"] == test_task]["renamed"]:
-        file_name = file_name + ".c3d"
-        c3d_path = os.path.join(dir, file_name)
-        temp_data, marker_sequence, problematic_markers = read_c3d_file(c3d_path).check_necessary_markers(test_task)
-        if len(problematic_markers) != 0:
-            print(file_name)
-            print(problematic_markers.reverse())
-            # break
-        # if data is None:
-        #     data = temp_data
-        # else:
-        #     data = pd.concat([data, temp_data], axis = 0)
+
+    # get train dataset
+    X = None
+    for i in range(len(train_metadata)):
+        file_name = train_metadata.loc[i, "renamed"] + ".c3d"
+        task_name = train_metadata.loc[i, "task"]
+        if task_name not in c3d_function.mapping_task_to_color.keys():
+            continue
+        if task_name != test_task:
+            continue
+        else:
+            print(file_name, task_name)
+            c3d_path = os.path.join(train_dir, file_name)
+            c3d_function.read_c3d(c3d_path)
+            temp_data, marker_sequence, problematic_markers = c3d_function.check_necessary_markers(task_name)
+            train_data, scaling = c3d_function.preprocess(temp_data, minmax = True)
+            if X is None:
+                X = train_data
+            else:
+                X = pd.concat([X, train_data], axis = 0)
+    
+    for i in range(len(test_metadata)):
+        file_name = test_metadata.loc[i, "renamed"] + ".c3d"
+        task_name = test_metadata.loc[i, "task"]
+        if task_name not in c3d_function.mapping_task_to_color.keys():
+            continue
+        if task_name != test_task:
+            continue
+        else:
+            print(file_name, task_name)
+            c3d_path = os.path.join(test_dir, file_name)
+            c3d_function.read_c3d(c3d_path)
+            temp_data, marker_sequence, problematic_markers = c3d_function.check_necessary_markers(task_name)
+            test_data, scaling = c3d_function.preprocess(temp_data, minmax = True)
+            if y is None:
+                y = test_data
+            else:
+                y = pd.concat([X, test_data], axis = 0)
 
 
